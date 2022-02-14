ca801da8adc6957273231c7a44497b9d92caac78
Maksim ilyukovich <m.ilyukovich@tinkoff.ru>
2022-02-14 23:27:29 +0300

    step 2 : fix empty lines

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -7,6 +7,7 @@ def load_url(url: string) -> string:
     page = requests.get(url)
     return page.text
 
+
 def load_image_url(url: string) -> string:
     page = requests.get(url)
     return page.content
